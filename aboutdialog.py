ccc62ea4abe066a1bf8658ae735f0b45c00138ff
notnypical <46350569+notnypical@users.noreply.github.com>
2020-08-09 12:55:53 +0200

    Process dialog close event

--- aboutdialog.py
+++ aboutdialog.py
@@ -65,3 +65,15 @@ class AboutDialog(QDialog):
         settings = QSettings()
 
         settings.setValue('AboutDialog/geometry', self.saveGeometry())
+
+
+    def closeEvent(self, event):
+        """
+        Processes the close event.
+
+        Args:
+            event (QCloseEvent): The close event.
+        """
+
+        self.writeSettings()
+        event.accept()
