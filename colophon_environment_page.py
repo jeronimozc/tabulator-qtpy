1ecbfd1c784cd0427b9345cc33508e102318a6d5
notnypical <46350569+notnypical@users.noreply.github.com>
2020-12-18 19:56:07 +0100

    Refactor 'Colophon Environment Page' widget

--- colophon_environment_page.py
+++ colophon_environment_page.py
@@ -2,20 +2,20 @@
 #
 # Copyright 2020 NotNypical, <https://notnypical.github.io>.
 #
-# This file is part of pyTabulator.
+# This file is part of Tabulator-QtPy.
 #
-# pyTabulator is free software: you can redistribute it and/or modify
+# Tabulator-QtPy is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# pyTabulator is distributed in the hope that it will be useful,
+# Tabulator-QtPy is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with pyTabulator.  If not, see <https://www.gnu.org/licenses/>.
+# along with Tabulator-QtPy.  If not, see <https://www.gnu.org/licenses/>.
 #
 
 import sys
@@ -25,18 +25,15 @@ from PySide2.QtCore import QSysInfo
 from PySide2.QtWidgets import QApplication, QFrame, QTextBrowser, QVBoxLayout, QWidget
 
 
-class ColophonEnvironmentWidget(QWidget):
+class ColophonEnvironmentPage(QWidget):
 
     def __init__(self, parent=None):
-        """
-        Initializes the ColophonEnvironmentWidget class.
-        """
-        super(ColophonEnvironmentWidget, self).__init__(parent)
+        super().__init__(parent)
 
         pythonVersion = sys.version
         pysideVersion = PySide2.__version__
-        qtVersion = PySide2.QtCore.qVersion() # Qt version used to run Qt for Python
-        qtBuildVersion = PySide2.QtCore.__version__ # Qt version used to compile PySide2
+        qtVersion = PySide2.QtCore.qVersion()  # Qt version used to run Qt for Python
+        qtBuildVersion = PySide2.QtCore.__version__  # Qt version used to compile PySide2
         osName = QSysInfo.prettyProductName()
         osKernelVersion = QSysInfo.kernelVersion()
         osCpuArchitecture = QSysInfo.currentCpuArchitecture()
@@ -45,7 +42,7 @@ class ColophonEnvironmentWidget(QWidget):
         textBox.setFrameStyle(QFrame.NoFrame)
         textBox.setStyleSheet('background-color:transparent;')
         textBox.setOpenExternalLinks(True)
-        textBox.setHtml(f'''<html><body><dl>
+        textBox.setHtml(self.tr(f'''<html><body><dl>
             <dt><strong>Application version</strong></dt>
                 <dd>{QApplication.applicationVersion()}</dd>
             <dt><strong>Qt for Python version</strong></dt>
@@ -54,17 +51,18 @@ class ColophonEnvironmentWidget(QWidget):
                 <dd>{pythonVersion}</dd>
             <dt><strong>Operation System</strong></dt>
                 <dd>{osName} (Kernel {osKernelVersion} on {osCpuArchitecture})</dd>
-            </dl></body></html>''')
+            </dl></body></html>'''))
 
         # Main layout
-        layout = QVBoxLayout()
-        layout.addWidget(textBox, 1)
-
-        self.setLayout(layout)
+        self.layout = QVBoxLayout(self)
+        self.layout.addWidget(textBox)
 
 
     def title(self):
-        """
-        Returns title of the widget.
-        """
-        return 'Environment'
+
+        return self.tr('Environment')
+
+
+    def setZeroMargins(self):
+
+        self.layout.setContentsMargins(0, 0, 0, 0)
