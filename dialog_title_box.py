7b2e744f6d892e540a9d11c3dd8c299f142f01b8
notnypical <46350569+notnypical@users.noreply.github.com>
2020-12-18 21:37:01 +0100

    Refactor 'Dialog Title Box' widget

--- dialog_title_box.py
+++ dialog_title_box.py
@@ -2,56 +2,53 @@
 #
 # Copyright 2020 NotNypical, <https://notnypical.github.io>.
 #
-# This file is part of pyTabulator.
+# This file is part of Tabulator-QtPy.
 #
-# pyTabulator is free software: you can redistribute it and/or modify
+# Tabulator-QtPy is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# pyTabulator is distributed in the hope that it will be useful,
+# Tabulator-QtPy is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with pyTabulator.  If not, see <https://www.gnu.org/licenses/>.
+# along with Tabulator-QtPy.  If not, see <https://www.gnu.org/licenses/>.
 #
 
 from PySide2.QtSvg import QSvgWidget
 from PySide2.QtWidgets import QApplication, QHBoxLayout, QLabel, QVBoxLayout, QWidget
 
+import resources
+
 
 class DialogTitleBox(QWidget):
 
     def __init__(self, parent=None):
-        """
-        Initializes the DialogTitleBox class.
-        """
-        super(DialogTitleBox, self).__init__(parent)
-
-        name = QLabel(f'<strong style="font-size:large;">{QApplication.applicationName()}</strong> v{QApplication.applicationVersion()}')
-        description = QLabel('A CSV editor written in Qt for Python.')
-
-        widgetTmp = QWidget()
-        vboxlayoutTmp = QVBoxLayout(widgetTmp)
-        vboxlayoutHeight = name.sizeHint().height() + vboxlayoutTmp.layout().spacing() + description.sizeHint().height()
+        super().__init__(parent)
 
         logo = QSvgWidget()
         logo.load(':/icons/apps/22/tabulator.svg')
-        logo.setFixedSize(vboxlayoutHeight, vboxlayoutHeight)
+
+        name = QLabel(self.tr(f'<strong style="font-size:large;">{QApplication.applicationName()}</strong> v{QApplication.applicationVersion()}'))
+        description = QLabel(self.tr('An editor tool for documents with character-separated values'))
 
         labels = QVBoxLayout()
         labels.addWidget(name)
         labels.addWidget(description)
 
-        titleBox = QHBoxLayout()
-        titleBox.addWidget(logo)
-        titleBox.addLayout(labels)
-
         # Main layout
-        layout = QVBoxLayout()
-        layout.addLayout(titleBox)
-        layout.addStretch(1)
+        self.layout = QHBoxLayout(self)
+        self.layout.addWidget(logo)
+        self.layout.addLayout(labels)
 
-        self.setLayout(layout)
+        # Set logo size
+        height = name.sizeHint().height() + labels.layout().spacing() + description.sizeHint().height()
+        logo.setFixedSize(height, height)
+
+
+    def setZeroMargins(self):
+
+        self.layout.setContentsMargins(0, 0, 0, 0)
