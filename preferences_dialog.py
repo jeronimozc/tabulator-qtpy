384586c98529c911aaa8b85f2c931dd4f5a79b4a
notnypical <46350569+notnypical@users.noreply.github.com>
2020-12-20 16:15:31 +0100

    Rename widget to 'Preferences General Settings'

--- preferences_dialog.py
+++ preferences_dialog.py
@@ -22,8 +22,8 @@ from PySide2.QtCore import QByteArray, QRect
 from PySide2.QtWidgets import (QApplication, QDialog, QDialogButtonBox, QHBoxLayout,
                                QListWidget, QStackedWidget, QVBoxLayout, QWidget)
 
-from preferences_application_widget import PreferencesApplicationWidget
 from preferences_document_widget import PreferencesDocumentWidget
+from preferences_general_settings import PreferencesApplicationWidget
 from settings import Settings
 
 
