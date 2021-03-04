539793c6ed54284a60fa520b3522183584969e7c
notnypical <46350569+notnypical@users.noreply.github.com>
2021-03-04 23:39:10 +0100

    Add basic document

---
+++ document.py
@@ -0,0 +1,39 @@
+# This Python file uses the following encoding: utf-8
+#
+# Copyright 2020-2021 NotNypical, <https://notnypical.github.io>.
+#
+# This file is part of Tabulator-QtPy, <https://github.com/notnypical/tabulator-qtpy>.
+#
+# Tabulator-QtPy is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Tabulator-QtPy is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Tabulator-QtPy.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+from PySide2.QtCore import Qt
+from PySide2.QtWidgets import QWidget
+
+
+class Document(QWidget):
+
+    _preferences = Preferences()
+
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.setAttribute(Qt.WA_DeleteOnClose)
+
+
+    def setPreferences(self, preferences):
+
+        self._preferences = preferences
+
