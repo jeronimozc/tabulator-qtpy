0a85179cc39dc60b30130c4b8d53c70c165b2343
notnypical <46350569+notnypical@users.noreply.github.com>
2021-03-04 23:42:18 +0100

    Fix application name

--- document_table_header_dialog.py
+++ document_table_header_dialog.py
@@ -2,20 +2,20 @@
 #
 # Copyright 2020-2021 NotNypical, <https://notnypical.github.io>.
 #
-# This file is part of pyTabulator, <https://github.com/notnypical/tabulator-qtpy>.
+# This file is part of Tabulator-QtPy, <https://github.com/notnypical/tabulator-qtpy>.
 #
-# pyTabulator is free software: you can redistribute it and/or modify
+# Tabulator-QtPy is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# pyTabulator is distributed in the hope that it will be useful,
+# Tabulator-QtPy is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with pyTabulator.  If not, see <https://www.gnu.org/licenses/>.
+# along with Tabulator-QtPy.  If not, see <https://www.gnu.org/licenses/>.
 #
 
 from PySide2.QtCore import Qt
